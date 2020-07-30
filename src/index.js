5f3af9cb866f3da67b87514c565b4b29bfc84971
nach66 <41806538+nach66@users.noreply.github.com>
2020-07-30 16:12:23 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { GithubProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain='dev-pu8wyk-g.us.auth0.com'
+      clientId='GYsVn60CkUU6fa4yiwpRrT1KVL5KuzPh'
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
